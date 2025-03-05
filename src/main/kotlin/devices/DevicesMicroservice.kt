5cccaff8cc25b3eb81d786e934a566dc51aeeadd
liza <liza692235@gmail.com>
2025-03-05 13:59:11 +0300

    calories added to devices microservice

--- src/main/kotlin/devices/DevicesMicroservice.kt
+++ src/main/kotlin/devices/DevicesMicroservice.kt
@@ -7,7 +7,9 @@ import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 import java.time.Duration
+import kotlin.math.roundToInt
 
 fun main() {
     embeddedServer(Netty, port = 8081, module = Application::myDevices).start(wait = true)
@@ -19,20 +21,37 @@ fun Application.myDevices() {
     }
 
     routing {
+        val stepsChannel = Channel<Int>(Channel.CONFLATED)
+
         webSocket("/steps") {
             var steps = 0
             while (true) {
                 delay(3000)
-                steps += (1..5).random()
+                val newSteps = (1..5).random()
+                steps += newSteps
+                stepsChannel.trySend(steps)
                 send("$steps")
             }
         }
+
         webSocket("/pulse") {
-            var pulse: Int
             while (true) {
                 delay(2000)
-                pulse = (55..75).random()
+                val pulse = (55..75).random()
                 send("$pulse")
+            }
+        }
+
+        webSocket("/calories") {
+            val weight = call.request.queryParameters["weight"]?.toDoubleOrNull() ?: 100.0
+            val stepLength = 0.75
+            val kcalPerKm = 0.65 * weight
+
+            while (true) {
+                val steps = stepsChannel.receive()
+                val distance = steps * stepLength / 1000
+                val calories = (distance * kcalPerKm).roundToInt()
+                send("$calories")
             }
         }
     }
